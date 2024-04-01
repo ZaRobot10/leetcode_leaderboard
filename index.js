e249bfd15b7a6a4fc7d9af77288a220066a0fb27
Ekankaar Khera <112088205+ZaRobot10@users.noreply.github.com>
2024-04-02 00:46:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,62 +13,120 @@ const port = 3000;
 // middleware
 app.use(express.static('public'));
 
+app.set('view engine', 'ejs');
 
 
 var userNames = ['kalpit04', '_Code_Shark', '8178458001' , 'za_robot10', 'dsharma02102004', 'varun9904', 'krishankant_nsut', 'D-01000100'];
 
+// var user_solved = [];
 
-
-app.get('/', async(req, res) => {
-
-
-    // for (var i = 0; i < userNames.length; i++) {
-        // const result = await axios.get(`https://alfa-leetcode-api.onrender.com/${userNames[i]}/solved`);
-
-        // var user = {
-        //     name: userNames[i],
-        //     easy: result.data.easySolved,
-        //     medium: result.data.mediumSolved,
-        //     hard: result.data.hardSolved,
-        //     total: result.data.totalSolved,
-        //     points : easy + 2 * medium + 4 * hard
-        // }
-
-        // user_solved.push(user);
-
-       
-    // }
- var user_solved = [];
-
-for (var i = 0; i < userNames.length; i++) {
-    var result =  await leetcode.user(userNames[i]);
+// for (var i = 0; i < userNames.length; i++) {
+//     var result =  await leetcode.user(userNames[i]);
    
 
 
-    var user = {
-        name: userNames[i],
+//     var user = {
+//         name: userNames[i],
+//         easy: result.matchedUser.submitStats.acSubmissionNum[1].count,
+//         medium: result.matchedUser.submitStats.acSubmissionNum[2].count,
+//         hard: result.matchedUser.submitStats.acSubmissionNum[3].count,
+//         total: result.matchedUser.submitStats.acSubmissionNum[0].count,
+//         points : result.matchedUser.submitStats.acSubmissionNum[1].count + 2 * result.matchedUser.submitStats.acSubmissionNum[2].count + 4 * result.matchedUser.submitStats.acSubmissionNum[3].count
+//     }
+
+
+//     console.log(user);
+
+//     user_solved.push(user);
+// }
+
+
+// // sort the user_solved array in descending order of points
+// user_solved.sort(function(a, b) {
+//     return b.points - a.points;
+// });
+
+
+// app.get('/', async(req, res) => {
+
+
+//     // for (var i = 0; i < userNames.length; i++) {
+//         // const result = await axios.get(`https://alfa-leetcode-api.onrender.com/${userNames[i]}/solved`);
+
+//         // var user = {
+//         //     name: userNames[i],
+//         //     easy: result.data.easySolved,
+//         //     medium: result.data.mediumSolved,
+//         //     hard: result.data.hardSolved,
+//         //     total: result.data.totalSolved,
+//         //     points : easy + 2 * medium + 4 * hard
+//         // }
+
+//         // user_solved.push(user);
+
+       
+//     // }
+
+//     var user_solved = [];
+
+// for (var i = 0; i < userNames.length; i++) {
+//     var result =  await leetcode.user(userNames[i]);
+   
+
+
+//     var user = {
+//         name: userNames[i],
+//         easy: result.matchedUser.submitStats.acSubmissionNum[1].count,
+//         medium: result.matchedUser.submitStats.acSubmissionNum[2].count,
+//         hard: result.matchedUser.submitStats.acSubmissionNum[3].count,
+//         total: result.matchedUser.submitStats.acSubmissionNum[0].count,
+//         points : result.matchedUser.submitStats.acSubmissionNum[1].count + 2 * result.matchedUser.submitStats.acSubmissionNum[2].count + 4 * result.matchedUser.submitStats.acSubmissionNum[3].count
+//     }
+
+
+//     console.log(user);
+
+//     user_solved.push(user);
+// }
+
+
+// // sort the user_solved array in descending order of points
+// user_solved.sort(function(a, b) {
+//     return b.points - a.points;
+// });
+   
+
+//     res.render('index.ejs', { user_solved: user_solved });
+// });
+
+app.get('/', async (req, res) => {
+    try {
+        const user_solved = await Promise.all(userNames.map(username => fetchUserData(username)));
+
+        // Sort the user_solved array in descending order of points
+        user_solved.sort((a, b) => b.points - a.points);
+
+        res.render('index', { user_solved });
+    } catch (error) {
+        console.error('Error fetching user data:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+async function fetchUserData(username) {
+    const result = await leetcode.user(username);
+    const user = {
+        name: username,
         easy: result.matchedUser.submitStats.acSubmissionNum[1].count,
         medium: result.matchedUser.submitStats.acSubmissionNum[2].count,
         hard: result.matchedUser.submitStats.acSubmissionNum[3].count,
         total: result.matchedUser.submitStats.acSubmissionNum[0].count,
-        points : result.matchedUser.submitStats.acSubmissionNum[1].count + 2 * result.matchedUser.submitStats.acSubmissionNum[2].count + 4 * result.matchedUser.submitStats.acSubmissionNum[3].count
-    }
-
-
-    console.log(user);
-
-    user_solved.push(user);
+        points: result.matchedUser.submitStats.acSubmissionNum[1].count + 
+                2 * result.matchedUser.submitStats.acSubmissionNum[2].count + 
+                4 * result.matchedUser.submitStats.acSubmissionNum[3].count
+    };
+    return user;
 }
-
-
-// sort the user_solved array in descending order of points
-user_solved.sort(function(a, b) {
-    return b.points - a.points;
-});
-   
-
-       res.render('index.ejs', { user_solved: user_solved });
-});
 
 
 
