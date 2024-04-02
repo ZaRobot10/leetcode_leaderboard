de07664b23a546e9d1dd180fd5e458989d4c3c92
Ekankaar Khera <112088205+ZaRobot10@users.noreply.github.com>
2024-04-02 21:11:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,100 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 
-var userNames = ['kalpit04', '_Code_Shark', '8178458001' , 'za_robot10', 'dsharma02102004', 'varun9904', 'krishankant_nsut', 'D-01000100'];
+var userNames = [
+    { user: 'kalpit04', problems: [5, 1, 2, 2] },
+    { user: '_Code_Shark', problems: [5, 1, 2, 2] },
+    { user: '8178458001', problems: [5, 1, 2, 2] },
+    { user: 'za_robot10', problems: [5, 1, 2, 2] },
+    { user: 'dsharma02102004', problems: [5, 1, 2, 2] },
+    { user: 'varun9904', problems: [5, 1, 2, 2] },
+    { user: 'krishankant_nsut', problems: [5, 1, 2, 2] },
+    { user: 'D-01000100', problems: [5, 1, 2, 2] },
+    { user: 's1marjeet_singh', problems: [5, 1, 2, 2] }
+];
+
+var previousDate =  "2024-3-25";
+
+app.get('/', async (req, res) => {
+
+
+        console.log('Fetching user data...');
+
+    try { 
+        
+        
+        const user_solved = await Promise.all(userNames.map(username => fetchUserData(username.user, username.problems)));
+
+        // Sort the user_solved array in descending order of points
+
+        
+        var currentDate = new Date();console.log(currentDate);
+        var prevDate = new Date(previousDate);
+
+        currentDate.setHours(0, 0, 0, 0);
+        prevDate.setHours(0, 0, 0, 0);
+
+        var differenceInMilliseconds = currentDate - prevDate;
+
+        // Convert milliseconds to days
+        var differenceInDays = differenceInMilliseconds / (1000 * 3600 * 24);
+        console.log(differenceInDays);
+
+        console.log(prevDate);
+        console.log(currentDate);
+        
+        // Check if the difference is 7 days or more
+        if (differenceInDays >= 7) {
+            // Update prev with today's date
+            previousDate = currentDate.toISOString().slice(0, 10);
+
+            // update problems inside user_names with the new data && update the problems in userNames
+
+            for (var i = 0; i < user_solved.length; i++) {
+                user_solved[i].problems = [user_solved[i].total, user_solved[i].easy, user_solved[i].medium, user_solved[i].hard];
+                userNames[i].problems = [user_solved[i].total, user_solved[i].easy, user_solved[i].medium, user_solved[i].hard];
+
+
+
+            }
+
+            
+           
+        }
+
+               user_solved.sort((a, b) => b.points - a.points);
+
+        res.render('index', { user_solved });
+    } catch (error) {
+        console.error('Error fetching user data:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+async function fetchUserData(username, problems) {
+    const result = await leetcode.user(username);
+
+    console.log(result);
+    const user = {
+        name: username,
+        easy: result.matchedUser.submitStats.acSubmissionNum[1].count,
+        medium: result.matchedUser.submitStats.acSubmissionNum[2].count,
+        hard: result.matchedUser.submitStats.acSubmissionNum[3].count,
+        total: result.matchedUser.submitStats.acSubmissionNum[0].count,
+        points: result.matchedUser.submitStats.acSubmissionNum[1].count + 
+                2 * result.matchedUser.submitStats.acSubmissionNum[2].count + 
+                4 * result.matchedUser.submitStats.acSubmissionNum[3].count,
+        problems: problems
+    };
+    return user;
+}
+
+
+app.listen(port, () => {
+
+    console.log(`Server is running on port http://localhost:${port}`);
+});
+
 
 // var user_solved = [];
 
@@ -99,38 +192,3 @@ var userNames = ['kalpit04', '_Code_Shark', '8178458001' , 'za_robot10', 'dsharm
 //     res.render('index.ejs', { user_solved: user_solved });
 // });
 
-app.get('/', async (req, res) => {
-    try {
-        const user_solved = await Promise.all(userNames.map(username => fetchUserData(username)));
-
-        // Sort the user_solved array in descending order of points
-        user_solved.sort((a, b) => b.points - a.points);
-
-        res.render('index', { user_solved });
-    } catch (error) {
-        console.error('Error fetching user data:', error);
-        res.status(500).send('Internal Server Error');
-    }
-});
-
-async function fetchUserData(username) {
-    const result = await leetcode.user(username);
-    const user = {
-        name: username,
-        easy: result.matchedUser.submitStats.acSubmissionNum[1].count,
-        medium: result.matchedUser.submitStats.acSubmissionNum[2].count,
-        hard: result.matchedUser.submitStats.acSubmissionNum[3].count,
-        total: result.matchedUser.submitStats.acSubmissionNum[0].count,
-        points: result.matchedUser.submitStats.acSubmissionNum[1].count + 
-                2 * result.matchedUser.submitStats.acSubmissionNum[2].count + 
-                4 * result.matchedUser.submitStats.acSubmissionNum[3].count
-    };
-    return user;
-}
-
-
-
-app.listen(port, () => {
-
-    console.log(`Server is running on port http://localhost:${port}`);
-});
